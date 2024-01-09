66263bc2cd3169e6b3009f6564dbee2b80787246
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2024-01-09 10:20:14 -0300

    Add arquivos do curso 2

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,4 +1,4 @@
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes, group, query, stagger } from '@angular/animations';
 
 export const highlightedStateTrigger = trigger('highlightState', [
     state('default', style({
@@ -37,5 +37,131 @@ export const checkStateTrigger = trigger('checkState', [
         animate('400ms ease-in', style({
             transform: 'scale(0.4)'
         }))
+    ])
+])
+
+export const filterTrigger = trigger('filterAnimation', [
+    transition(':enter', [
+        style({
+            opacity: 0,
+            width: 0
+        }),
+        animate('1000ms ease-out', keyframes([
+            style({ offset: 0, opacity: 0, width: 0 }),
+            style({ offset: 0.8, opacity: 0.5, width: '*' }),
+            style({ offset: 1, opacity: 1, width: '*' }),
+        ]))
+    ]),
+    transition(':leave', [
+        animate('400ms ease-out', style({
+            opacity: 0,
+            width: 0
+        }))
+    ])
+])
+
+export const formButtonTrigger = trigger('formButton', [
+    transition('invalid => valid', [
+        query('#botao-salvar', [
+            group([
+                animate(200, style({
+                    backgroundColor: '#63B77C'
+                })),
+                animate(100, style({
+                    transform: 'scale(1.1)'
+                })),
+            ]),
+            animate(200, style({
+                transform: 'scale(1)'
+            }))
+        ]),
+    ]),
+    transition('valid => invalid', [
+        query('#botao-salvar', [
+            group([
+                animate(200, style({
+                    backgroundColor: '#6C757D'
+                })),
+                animate(100, style({
+                    transform: 'scale(1.1)'
+                })),
+            ]),
+            animate(200, style({
+                transform: 'scale(1)'
+            }))
+        ])
+    ])
+]);
+
+export const avisoFiltroTrigger = trigger('avisoFiltroAnimation', [
+    transition(':enter', [
+        style({
+            width: '100%',
+            transform: 'translateX(-100%)',
+            opacity: 0
+        }),
+        group([
+            animate('0.3s 0.1s ease', style({
+                transform: 'translateX(0)',
+                width: '*'
+            })),
+            animate('0.3s ease', style({
+                opacity: 1
+            }))
+        ])
+    ]),
+    transition(':leave', [
+        group([
+            animate('0.3s ease', style({
+                transform: 'translateX(100%)',
+                width: '*'
+            })),
+            animate('0.3s 0.2s ease', style({
+                opacity: 0
+            }))
+        ])
+    ]),
+]);
+
+export const shakeTrigger = trigger('shakeAnimation', [
+    transition('* => *', [
+        query('input.ng-invalid:focus, select.ng-invalid:focus', [
+            animate('0.5s', keyframes([
+                style({ border: '2px solid red' }),
+                style({ transform: 'translateX(-10px)' }),
+                style({ transform: 'translateX(10px)' }),
+                style({ transform: 'translateX(-10px)' }),
+                style({ transform: 'translateX(10px)' }),
+                style({ transform: 'translateX(-10px)' }),
+                style({ transform: 'translateX(10px)' }),
+                style({ transform: 'translateX(-10px)' }),
+                style({ transform: 'translateX(0px)' }),
+            ]))
+        ], { optional: true })
+    ])
+]);
+
+export const listStateTrigger = trigger('listState', [
+    transition('* => *', [
+        query(':enter', [
+            style({
+                opacity: 0,
+                transform: 'translateX(-100%)'
+            }),
+            stagger(200, [
+                animate('500ms ease-out', keyframes([
+                    style({
+                        opacity: 1,
+                        transform: 'translateX(15%)',
+                        offset: 0.4
+                    }),
+                    style({
+                        opacity: 1,
+                        transform: 'translateX(0)',
+                        offset: 1
+                    }),
+                ]))
+            ]),
+        ], {optional: true})
     ])
 ])
--- src/app/lista-tarefas/lista-tarefas.component.ts
+++ src/app/lista-tarefas/lista-tarefas.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { TarefaService } from 'src/app/service/tarefa.service';
 import { Tarefa } from '../interface/tarefa';
-import { checkStateTrigger, highlightedStateTrigger, showStateTrigger } from '../animations';
+import { avisoFiltroTrigger, checkStateTrigger, filterTrigger, formButtonTrigger, highlightedStateTrigger, listStateTrigger, shakeTrigger, showStateTrigger } from '../animations';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-lista-tarefas',
   templateUrl: './lista-tarefas.component.html',
   styleUrls: ['./lista-tarefas.component.css'],
   animations: [
-    highlightedStateTrigger, 
+    highlightedStateTrigger,
     showStateTrigger,
-    checkStateTrigger
+    checkStateTrigger,
+    filterTrigger,
+    formButtonTrigger,
+    avisoFiltroTrigger,
+    shakeTrigger,
+    listStateTrigger
   ]
 })
 export class ListaTarefasComponent implements OnInit {
@@ -23,6 +28,10 @@ export class ListaTarefasComponent implements OnInit {
   validado: boolean = false;
   indexTarefa: number = -1;
   id: number = 0;
+  campoBusca: string = '';
+  tarefasFiltradas: Tarefa[] = [];
+  tarefasSubscription: Subscription = new Subscription()
+  estadoBotao: string = 'unchecked'
 
   formulario: FormGroup = this.fomBuilder.group({
     id: [0],
@@ -34,15 +43,26 @@ export class ListaTarefasComponent implements OnInit {
 
   constructor(
     private service: TarefaService,
-    private router: Router,
     private fomBuilder: FormBuilder
-  ) {}
+  ) { }
 
-  ngOnInit(): Tarefa[] {
-    this.service.listar(this.categoria).subscribe((listaTarefas) => {
-      this.listaTarefas = listaTarefas;
-    });
-    return this.listaTarefas;
+  ngOnInit(): void {
+    this.service.listar()
+
+    this.tarefasSubscription = this.service.tarefas$.subscribe(tarefas => {
+      this.listaTarefas = tarefas;
+      this.tarefasFiltradas = tarefas;
+    })
+  }
+
+  filtrarTarefasPorDescricao(descricao: string) {
+    this.campoBusca = descricao.trim().toLowerCase();
+    if (descricao) {
+      this.tarefasFiltradas = this.listaTarefas.filter(tarefa =>
+        tarefa.descricao.toLowerCase().includes(this.campoBusca))
+    } else {
+      this.tarefasFiltradas = this.listaTarefas
+    }
   }
 
   mostrarOuEsconderFormulario() {
@@ -59,22 +79,24 @@ export class ListaTarefasComponent implements OnInit {
   }
 
   editarTarefa() {
-    this.service.editar(this.formulario.value).subscribe({
-      complete: () => this.atualizarComponente(),
-    });
+    if(this.formulario.valid) {
+      const tarefaEditada = this.formulario.value;
+      this.service.editar(tarefaEditada, true);
+      this.resetarFormulario();
+    }
   }
 
   criarTarefa() {
-    this.service.criar(this.formulario.value).subscribe({
-      complete: () => this.atualizarComponente(),
-    });
+    if(this.formulario.valid) {
+      const novaTarefa = this.formulario.value;
+      this.service.criar(novaTarefa);
+      this.resetarFormulario();
+    }
   }
 
-  excluirTarefa(id: number) {
-    if (id) {
-      this.service.excluir(id).subscribe({
-        complete: () => this.recarregarComponente(),
-      });
+  excluirTarefa(tarefa: Tarefa) {
+    if (tarefa.id) {
+      this.service.excluir(tarefa.id)
     }
   }
 
@@ -92,15 +114,6 @@ export class ListaTarefasComponent implements OnInit {
     });
   }
 
-  recarregarComponente() {
-    this.router.navigate(['/listaTarefas']);
-  }
-
-  atualizarComponente() {
-    this.recarregarComponente();
-    this.resetarFormulario();
-  }
-
   carregarParaEditar(id: number) {
     this.service.buscarPorId(id!).subscribe((tarefa) => {
       this.formulario = this.fomBuilder.group({
@@ -114,20 +127,16 @@ export class ListaTarefasComponent implements OnInit {
     this.formAberto = true;
   }
 
-  finalizarTarefa(id: number) {
-    this.id = id
-    this.service.buscarPorId(id!).subscribe((tarefa) => {
-      this.service.atualizarStatusTarefa(tarefa).subscribe(() => {
-        this.listarAposCheck();
-      });
-    });
+  finalizarTarefa(tarefa: Tarefa) {
+    this.id = tarefa.id;
+    this.service.atualizarStatusTarefa(tarefa);
+    if(tarefa.statusFinalizado === true) {
+      this.estadoBotao = 'checked'
+    } else {
+      this.estadoBotao = 'unchecked'
+    }
   }
 
-  listarAposCheck() {
-    this.service.listar(this.categoria).subscribe((listaTarefas) => {
-      this.listaTarefas = listaTarefas;
-    });
-  }
 
   habilitarBotao(): string {
     if (this.formulario.valid) {
